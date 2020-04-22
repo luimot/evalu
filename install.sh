83c91b934c1031847c3c49bb56d9f988df6fd4e4
luiz <luiz@localhost.localdomain>
2020-04-22 13:01:37 -0300

    Alterado código fonte

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 DIR_ATUAL=$(dirname "$0")
 ALIAS_PATH="alias evalu='bash ${DIR_ATUAL}/evalU.sh'"
+BRC_PATH="~/Coding/Git/x"
+STATE=cat $BRC_PATH |grep $ALIAS_PATH ==  ""
 chmod u+x evalU.sh
-if [[ cat ~/Coding/Git/x |grep ALIAS_PATH ==  "" ]]
+if [ $STATE ];then
+	echo $ALIAS_PATH >> ~/Coding/Git/x
+fi
+	
